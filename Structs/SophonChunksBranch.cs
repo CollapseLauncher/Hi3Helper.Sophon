78d2fa7772d824d5f8f8fad9a55b69b455e75157
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-06-09 23:12:04 +0700

    Add option to not throw if matching field is not found

--- Structs/SophonChunksBranch.cs
+++ Structs/SophonChunksBranch.cs
@@ -55,12 +55,21 @@ namespace Hi3Helper.Sophon
 #endif
         }
 
+        public static
+            Task<SophonChunkManifestInfoPair>
+            CreateSophonChunkManifestInfoPair(HttpClient client,
+                                              string url,
+                                              string matchingField = null,
+                                              CancellationToken token = default)
+            => CreateSophonChunkManifestInfoPair(client, url, matchingField, true, token);
+
         public static async
             Task<SophonChunkManifestInfoPair>
             CreateSophonChunkManifestInfoPair(HttpClient        client,
                                               string            url,
-                                              string            matchingField = null,
-                                              CancellationToken token         = default)
+                                              string            matchingField     = null,
+                                              bool              isThrowIfNotFound = true,
+                                              CancellationToken token             = default)
         {
             var sophonBranch = await GetSophonBranchInfo
 #if !NET6_0_OR_GREATER
@@ -93,29 +102,37 @@ namespace Hi3Helper.Sophon
                 sophonBranch.Data.ManifestIdentityList?
                    .FirstOrDefault(x => x.MatchingField == matchingField);
 
-            if (sophonManifestIdentity == null)
+            if (sophonManifestIdentity != null)
             {
-                throw new KeyNotFoundException($"Sophon manifest with matching field: {matchingField} is not found!");
+                return new SophonChunkManifestInfoPair
+                {
+                    ChunksInfo = CreateChunksInfo(sophonManifestIdentity.ChunksUrlInfo.UrlPrefix,
+                                                  sophonManifestIdentity.ChunkInfo.ChunkCount,
+                                                  sophonManifestIdentity.ChunkInfo.FileCount,
+                                                  sophonManifestIdentity.ChunksUrlInfo.IsCompressed,
+                                                  sophonManifestIdentity.ChunkInfo.UncompressedSize,
+                                                  sophonManifestIdentity.ChunkInfo.CompressedSize),
+                    ManifestInfo = CreateManifestInfo(sophonManifestIdentity.ManifestUrlInfo.UrlPrefix,
+                                                      sophonManifestIdentity.ManifestFileInfo.Checksum,
+                                                      sophonManifestIdentity.ManifestFileInfo.FileName,
+                                                      sophonManifestIdentity.ManifestUrlInfo.IsCompressed,
+                                                      sophonManifestIdentity.ManifestFileInfo.UncompressedSize,
+                                                      sophonManifestIdentity.ManifestFileInfo.CompressedSize),
+                    OtherSophonBuildData = sophonBranch.Data,
+                    MatchingField        = sophonManifestIdentity.MatchingField,
+                    CategoryName         = sophonManifestIdentity.CategoryName,
+                    CategoryId           = sophonManifestIdentity.CategoryId
+                };
             }
+
+            if (isThrowIfNotFound)
+                throw new KeyNotFoundException($"Sophon manifest with matching field: {matchingField} is not found!");
 
             return new SophonChunkManifestInfoPair
             {
-                ChunksInfo = CreateChunksInfo(sophonManifestIdentity.ChunksUrlInfo.UrlPrefix,
-                                              sophonManifestIdentity.ChunkInfo.ChunkCount,
-                                              sophonManifestIdentity.ChunkInfo.FileCount,
-                                              sophonManifestIdentity.ChunksUrlInfo.IsCompressed,
-                                              sophonManifestIdentity.ChunkInfo.UncompressedSize,
-                                              sophonManifestIdentity.ChunkInfo.CompressedSize),
-                ManifestInfo = CreateManifestInfo(sophonManifestIdentity.ManifestUrlInfo.UrlPrefix,
-                                                  sophonManifestIdentity.ManifestFileInfo.Checksum,
-                                                  sophonManifestIdentity.ManifestFileInfo.FileName,
-                                                  sophonManifestIdentity.ManifestUrlInfo.IsCompressed,
-                                                  sophonManifestIdentity.ManifestFileInfo.UncompressedSize,
-                                                  sophonManifestIdentity.ManifestFileInfo.CompressedSize),
-                OtherSophonBuildData = sophonBranch.Data,
-                MatchingField        = sophonManifestIdentity.MatchingField,
-                CategoryName         = sophonManifestIdentity.CategoryName,
-                CategoryId           = sophonManifestIdentity.CategoryId
+                IsFound       = false,
+                ReturnCode    = 404,
+                ReturnMessage = $"Sophon manifest with matching field: {matchingField} is not found!"
             };
         }
     }
